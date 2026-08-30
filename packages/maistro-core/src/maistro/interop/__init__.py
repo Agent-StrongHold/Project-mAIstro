3f2e7062a2b15251bfe8993de6d3b92cfc4e5537
Blake Matthews <270501485+BlakeMatthews-dev@users.noreply.github.com>
2026-08-30 18:29:50 -0500

    Export shared interoperability contract

---
+++ packages/maistro-core/src/maistro/interop/__init__.py
@@ -0,0 +1,15 @@
+"""Shared cross-product interoperability contract (#458)."""
+
+from maistro.interop.contract import (
+    INTEROP_ONTOLOGY_V1,
+    ConceptSpec,
+    InteropContractError,
+    InteropOntology,
+)
+
+__all__ = [
+    "INTEROP_ONTOLOGY_V1",
+    "ConceptSpec",
+    "InteropContractError",
+    "InteropOntology",
+]
